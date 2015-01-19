2be453de40e6d453be9189fa992899e3e76face3
Cedric TESNIERE <tesniere.cedric@gmail.com>
2015-01-19 15:42:29 +0100

    Generate Goban with Board class

--- js/board.js
+++ js/board.js
@@ -9,6 +9,7 @@ Board.prototype.resetStones = function () {
     for (var x = 0; x < this.size; x++) {
         this.stones[x] = [];
     }
+    this.generateGoban();
 };
 
 Board.prototype.get = function (x, y) {
@@ -55,4 +56,38 @@ Board.prototype.init = function (goban) {
 Board.prototype.isCoordOnGoban = function (x, y) {
     return (0 <= x && x < this.size)
         && (0 <= y && y < this.size);
+};
+
+Board.prototype.generateGoban = function () {
+    var r = document.getElementById('canvasGoban');
+    for (var row = 0; row < this.size; row++) {
+        for (var col = 0; col < this.size; col++) {
+            var attrClass = [];
+            if (col == 0) {
+                attrClass.push("first_col");
+            }
+            if (col == this.size - 1) {
+                attrClass.push("last_col");
+            }
+            if (row == 0) {
+                attrClass.push("first_row");
+            }
+            if (row == this.size - 1) {
+                attrClass.push("last_row");
+            }
+            if (row == 2 && col == 2) {
+                attrClass.push("oeil");
+            }
+            if (row == 6 && col == 2) {
+                attrClass.push("oeil");
+            }
+            if (row == 2 && col == 6) {
+                attrClass.push("oeil");
+            }
+            if (row == 6 && col == 6) {
+                attrClass.push("oeil");
+            }
+            r.innerHTML = r.innerHTML + "<a><div id='" + row + col + "' class=\"cell " + attrClass.join(" ") + "\"></div></a>";
+        }
+    }
 };
