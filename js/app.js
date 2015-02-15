b846cd8c348c1628db6abac9a478fcef432edeea
Cedric TESNIERE <tesniere.cedric@gmail.com>
2015-02-15 15:10:55 +0100

    adding of protactor.js

--- js/app.js
+++ js/app.js
@@ -7,31 +7,36 @@ var App = function (url) {
  * Initalize the application
  */
 App.prototype.init = function (url) {
-    var gameId = window.location.hash.substring(1);
+    var self = this;
+    var routes = {
+        '/:gameId': function (gameId) {
+            if (!Utils.checkFirebaseId(gameId)) {
+                window.location.replace('/#/');
+            } else {
+                self.createGame(url, gameId);
+            }
+        },
+        '/': function () {
+            self.firebase = new FB(url, null);
+            var welcome = new Welcome(self.firebase);
+            welcome.listGames();
 
-    if (!_.isEmpty(gameId) && Utils.checkFirebaseId(gameId)) {
-        this.createGame(url, gameId);
-    } else {
-        window.location.replace("/#");
-        this.firebase = new FB(url, null);
-        var welcome = new Welcome(this.firebase);
-        welcome.listGames();
-
-        var self = this;
-        welcome.watchNewGame().then(function (size) {
-            var gameId = self.firebase.newGame(size);
-            self.createGame(url, gameId);
-        });
-    }
+            welcome.watchNewGame().then(function (size) {
+                var gameId = self.firebase.newGame(size);
+                window.location.replace('/#/' + gameId);
+            });
+        }
+    };
+    var router = Router(routes);
+    router.init('/');
 };
 
 App.prototype.createGame = function (url, gameId) {
-    window.location.replace("/#" + gameId);
     this.firebase = new FB(url, gameId);
     var self = this;
     this.firebase.once('games/' + gameId + '/size', 'value').then(function (snap) {
         var size = snap.val();
-        console.debug("new Game [size : ", size, ", gameId : ", gameId, "]");
+        console.debug('new Game [size : ', size, ', gameId : ', gameId, ']');
 
         new Game(self.firebase, url, gameId, size);
     });
